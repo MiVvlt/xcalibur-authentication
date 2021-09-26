bda39848fb51c9f4340dd74a66224b2cfaa61739
Michael <30836085+MiVvlt@users.noreply.github.com>
2021-09-26 19:30:11 +0200

    changed transport to redis

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,4 @@
-import { Logger } from '@nestjs/common';
 import * as dotenv from 'dotenv';
-
 dotenv.config();
 import { NestFactory } from '@nestjs/core';
 import {
@@ -8,18 +6,18 @@ import {
 	MicroserviceOptions,
 } from '@nestjs/microservices';
 import { AuthModule } from './auth.module';
+import { Logger } from '@nestjs/common';
+
 
 async function bootstrap() {
-	const options = {
-		host: process.env.SERVICE_HOST || 'localhost',
-		port: parseInt( process.env.SERVICE_PORT ) || 3000,
-	};
 
 	const app = await NestFactory.createMicroservice<MicroserviceOptions>( AuthModule, {
-		transport: Transport.TCP,
-		options,
+		transport: Transport.REDIS,
+		options: {
+			url: process.env.REDIS_URL
+		}
 	} );
-	new Logger( 'Bootstrap' ).log( `Authentication microservice running at ${ options.host }:${ options.port }` );
+	new Logger('Bootstrap').log(`connecting service to redis: ${process.env.REDIS_URL}`)
 	app.listen();
 }
 
